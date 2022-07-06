f4bb771841ee9002611a1c76c20336dd25cf4138
paveldeuce <deucela.pavel@gmail.com>
2022-07-06 14:23:23 +0300

    completed third step: added relevance

--- src/searchEngine.js
+++ src/searchEngine.js
@@ -4,7 +4,7 @@ const buildSearchEngine = (items) => {
 
   return {
     search: (searchStr) => {
-      const result = [];
+      const documentsIds = [];
       const byWords = items.map((item) => {
         const words = item.text.split(' ');
         return {
@@ -17,11 +17,23 @@ const buildSearchEngine = (items) => {
         item.words.forEach((word) => {
           const token = new RegExp(word, 'gi');
           const term = searchStr.match(token);
-          if (term !== null && term.length >= 0 && !result.includes(item.id)) {
-            result.push(item.id);
+          if (term !== null && term.length >= 0) {
+            documentsIds.push(item.id);
           }
         });
       });
+
+      const byRelevance = documentsIds.reduce(
+        (acc, id) => (acc[id] ? { ...acc, [id]: acc[id] + 1 } : { ...acc, [id]: 1 }),
+        {},
+      );
+      const result = Object.entries(byRelevance)
+        .map(([key, value]) => ({
+          id: key,
+          count: value,
+        }))
+        .sort((a, b) => (a.count <= b.count ? 1 : -1))
+        .map((i) => i.id);
 
       return result;
     },
